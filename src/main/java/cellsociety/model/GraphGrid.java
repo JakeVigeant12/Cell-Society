a1b0581660857c2fc6f20286cca71b21ff99ba96
Jake Vigeant <jmv43@duke.edu>
2022-10-17 16:02:07 -0400

    skeleton of graph construction

--- src/main/java/cellsociety/model/GraphGrid.java
+++ src/main/java/cellsociety/model/GraphGrid.java
@@ -22,33 +22,44 @@ public class GraphGrid extends Grid{
     //TODO refactor method to generalize neighbor calculation using open/close solution
     //Used to ID the cells as they are created for ease of access, upper left is 1, lower right is max
     int cellCount = 0;
-    for(ArrayList<String> currRow : inputLayout){
+    for(int i = 0; i < inputLayout.size(); i++){
       //TODO: Implemented enum switch for now, refactor using abstract factory design pattern after functional
-      for(String cellData : currRow){
+      for(int j = 0; j < inputLayout.get(i).size(); j++){
         cellCount++;
-        Cell newCell;
+        Cell newCell = null;
+        Integer cellData  = Integer.parseInt(inputLayout.get(i).get(j));
         switch(simType) {
           case GameOfLife:
-            newCell = new GameOfLifeCell(Integer.parseInt(cellData,cellCount));
+            newCell = new GameOfLifeCell(cellData,cellCount);
             break;
           case Fire:
+            //TODO: Implement simParameters this is a dummy value
+            newCell = new FireCell(cellData,cellCount, 0.1);//Get pcatch
             break;
+            //TODO: implmentations for these other cells
           case Segregation:
+            newCell = new GameOfLifeCell(cellData,cellCount);
             break;
           case WatorWorld:
+            newCell = new GameOfLifeCell(cellData,cellCount);
             break;
           case RockPaperScissors:
+            newCell = new GameOfLifeCell(cellData,cellCount);
             break;
           case Percolation:
+            newCell = new GameOfLifeCell(cellData,cellCount);
             break;
         }
-
-
+        myAdjacenyList.put(newCell, initializeNeighbors(inputLayout,i,j));
+        myCells.put(cellCount,newCell);
       }
-
-
     }
+  }
+  public ArrayList<Cell> initializeNeighbors(ArrayList<ArrayList<String>> gridParsing, int row, int col){
+      ArrayList<Cell> neighbors = new ArrayList<>();
 
 
+
+      return neighbors;
   }
 }
