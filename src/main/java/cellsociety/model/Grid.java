196c70fb80be939fd617fb672545ca455c470d5e
Jake Vigeant <jmv43@duke.edu>
2022-10-17 06:09:34 -0400

    typo

--- src/main/java/cellsociety/model/Grid.java
+++ src/main/java/cellsociety/model/Grid.java
@@ -4,11 +4,6 @@ import java.util.*;
 
 //abstraction of grid to allow flexibility in implementation
 public abstract class Grid {
-
-  private HashMap<Integer, Cell> myCells;
-  private HashMap<Cell, ArrayList<Cell>> myAdjacenyList;
-
-
   public void computeStates() {
     return;
   }
