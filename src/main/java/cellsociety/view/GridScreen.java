bed2638dc144081d81620ba69b81b8ce7eb1776b
Eka Ebong <eae25@duke.edu>
2022-10-17 16:43:57 -0400

    formated the grid screen more

--- src/main/java/cellsociety/view/GridScreen.java
+++ src/main/java/cellsociety/view/GridScreen.java
@@ -23,7 +23,7 @@ public class GridScreen extends SceneCreator{
     private Text author;
     private Text description;
     private Text Abouttitle;
-    private Paint jid = Color.GRAY;
+    private Paint jid = Color.LIGHTGRAY;
     private ResourceBundle myLabels;
 
 
@@ -34,28 +34,41 @@ public class GridScreen extends SceneCreator{
 
     public Pane createGridScreen(Stage stage, ResourceBundle label){
         myLabels = label;
-        play = new Button(">");
-        step = new Button(">|");
+        play = new Button("Play");
+        step = new Button("Next");
         play.getStyleClass().add("buttons");
-
-        Rectangle about = new Rectangle(200,600);
-        about.setX(600);
-        about.setFill(jid);
 
         Abouttitle = new Text(myLabels.getString("aboutText"));
         title = new Text(myLabels.getString("title"));
         type = new Text(myLabels.getString("typeText"));
         author = new Text(myLabels.getString("authorText"));
         description = new Text(myLabels.getString("descriptionText"));
+        Abouttitle.getStyleClass().add("title");
+        title.getStyleClass().add("info");
+        type.getStyleClass().add("info");
+        author.getStyleClass().add("info");
+        description.getStyleClass().add("info");
 
         VBox fileinfo = new VBox(Abouttitle, title, type, author, description);
         fileinfo.setBackground(Background.fill(jid));
+        fileinfo.getStyleClass().add("aboutbox");
+
         HBox controls = new HBox(play,step);
         controls.setLayoutX(400);
         controls.setLayoutY(690);
         controls.getStyleClass().add("allbuttons");
-        gridPane.getChildren().addAll(controls, about, fileinfo);
+
+        gridPane.getChildren().addAll(controls, fileinfo);
         return gridPane;
+    }
+
+    public void handleButtons(Stage stage){
+        play.setOnAction(event -> {}
+                //call the grid view once every 60 seconds
+                );
+        step.setOnAction(event ->{}
+        //{ update grid once}
+        );
     }
 
 
