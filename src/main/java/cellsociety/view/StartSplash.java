c21b1dd74582e85504a9635d503becc1c507954f
Nick Ward <nw121@duke.edu>
2022-10-18 11:47:15 -0400

    added comments and fixed up some basic syntax errors

--- src/main/java/cellsociety/view/StartSplash.java
+++ src/main/java/cellsociety/view/StartSplash.java
@@ -1,7 +1,5 @@
 package cellsociety.view;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -13,17 +11,17 @@ import javafx.scene.control.Button;
 import java.util.ResourceBundle;
 
 public class StartSplash extends SceneCreator {
-    public double screenSize;
     public Pane startPane;
     private Text mainTitle;
     private Text selectLanguage;
-    private Button english;
-    private Button spanish;
-    private Button anotherlanguage;
+    private Button englishButton;
+    private Button spanishButton;
+    private Button frenchButton;
 
     private ImageView myBackground;
 
-    ResourceBundle startinfo = ResourceBundle.getBundle("startinfo");
+    ResourceBundle startInfo = ResourceBundle.getBundle("startInfo");
+
 
     public StartSplash(double size){
         super(size);
@@ -32,9 +30,9 @@ public class StartSplash extends SceneCreator {
     }
 
     public Pane createStart(Stage stage){
-        english = makeButton("englishText");
-        spanish = makeButton("spanishText");
-        anotherlanguage = makeButton("thirdText");
+        englishButton = makeButton("englishText");
+        spanishButton = makeButton("spanishText");
+        frenchButton = makeButton("frenchText");
 
         mainTitle = new Text("Team 10");
         mainTitle.getStyleClass().add("mainTitle");
@@ -46,13 +44,11 @@ public class StartSplash extends SceneCreator {
         selectLanguage.setLayoutX(240);
         selectLanguage.setLayoutY(460);
 
-        myBackground.setImage(new Image(startinfo.getString("startgif")));
+        myBackground.setImage(new Image(startInfo.getString("startgif")));
         myBackground.setFitWidth(mySize);
         myBackground.setFitHeight(mySize);
 
-        //
-
-        HBox buttons = new HBox(english, spanish, anotherlanguage);
+        HBox buttons = new HBox(englishButton, spanishButton, frenchButton);
         buttons.setLayoutY(490);
         buttons.setLayoutX(130);
         buttons.getStyleClass().add("allbuttons");
@@ -65,7 +61,7 @@ public class StartSplash extends SceneCreator {
 
     public Button makeButton(String property) {
         Button result = new Button();
-        String label = startinfo.getString(property);
+        String label = startInfo.getString(property);
         result.setText(label);
         result.setId(property);
         return result;
@@ -73,17 +69,14 @@ public class StartSplash extends SceneCreator {
 
     public void handleEvents(Stage stage){
         FileInput fi = new FileInput(mySize);
-        english.setOnAction(event->{
+        englishButton.setOnAction(event->{
             stage.setScene(createScene(stage, fi.createFileInput(stage, "LangLabels"), "fileinput.css"));
-            //nextScreen(stage);
         });
-        spanish.setOnAction(event ->{
+        spanishButton.setOnAction(event ->{
             stage.setScene(createScene(stage, fi.createFileInput(stage, "SpanishLabels"), "fileinput.css"));
-            nextScreen(stage);
         });
-        anotherlanguage.setOnAction(event -> {
+        frenchButton.setOnAction(event -> {
             stage.setScene(createScene(stage, fi.createFileInput(stage, "FrenchLabels"), "fileinput.css"));
-//            nextScreen(stage);
         });
     }
 
--- src/test/java/cellsociety/view/StartSplashTest.java
+++ src/test/java/cellsociety/view/StartSplashTest.java
@@ -5,15 +5,13 @@ import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StartSplashTest extends DukeApplicationTest {
     @Override
     public void start (Stage stage) {
 
         StartSplash ss = new StartSplash(600.0);
         SceneCreator current = new SceneCreator(600.0);
-        stage.setScene(current.createScene(stage,ss.createStart(stage),"startsplash.css" ));
+        stage.setScene(current.createScene(stage,ss.createStart(stage), "startSplash.css"));
         stage.show();
     }
 
