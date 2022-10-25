4add1774903d8b3c93f40d212dbdec5a42c39e16
Jake Vigeant <jmv43@duke.edu>
2022-10-24 21:37:35 -0400

    chose to implement wator with a separate grid that overrides functions but has the same interfacing capabilities

---
+++ src/main/java/cellsociety/model/grids/WatorGraphGrid.java
@@ -0,0 +1,18 @@
+package cellsociety.model.grids;
+
+import cellsociety.view.GridWrapper;
+import java.util.Properties;
+
+public class WatorGraphGrid extends GraphGrid{
+
+  /**
+   * Constructor for GraphGrid class
+   *
+   * @param gridParsing is the layout of the grid
+   * @param properties
+   */
+  public WatorGraphGrid(GridWrapper gridParsing, Properties properties) {
+    super(gridParsing, properties);
+  }
+
+}
