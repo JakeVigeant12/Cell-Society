7539796ce20aebf8e8dd845f4334819bb7c6881f
Jake Vigeant <jmv43@duke.edu>
2022-10-30 22:47:20 -0400

    segregation grid tests

--- src/test/java/cellsociety/model/SegregationGraphGridTest.java
+++ src/test/java/cellsociety/model/SegregationGraphGridTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import cellsociety.model.grids.FireGraphGrid;
 import cellsociety.model.grids.GameOfLifeGraphGrid;
+import cellsociety.model.grids.SegregationGraphGrid;
 import cellsociety.parser.CSVParser;
 import cellsociety.view.GridWrapper;
 import java.util.ArrayList;
@@ -13,13 +14,13 @@ import org.junit.jupiter.api.Test;
 
 public class SegregationGraphGridTest {
   CSVParser myGridParser = new CSVParser();
-  GridWrapper gridWrapper = myGridParser.parseData("schelling/schelling_simple_test.csv");
+  GridWrapper gridWrapper = myGridParser.parseData("schelling/schelling_simple_stable.csv");
   Properties p = new Properties();
   @Test
   public void testStateComputation()
   {
     p.setProperty("Type", "Segregation");
-    GameOfLifeGraphGrid myTestGrid = new GameOfLifeGraphGrid(gridWrapper, p);
+    SegregationGraphGrid myTestGrid = new SegregationGraphGrid(gridWrapper, p);
     ArrayList<Integer> prevStates = new ArrayList<>();
     ArrayList<Integer> nextStates = new ArrayList<>();
     prevStates = (ArrayList) myTestGrid.representStatesAsList(myTestGrid.getCells());
@@ -31,5 +32,18 @@ public class SegregationGraphGridTest {
     }
     //Stable input config
     assertTrue(areEqual);
+
+    gridWrapper = myGridParser.parseData("schelling/schelling_simple_unstable.csv");
+    myTestGrid = new SegregationGraphGrid(gridWrapper, p);
+    prevStates = (ArrayList) myTestGrid.representStatesAsList(myTestGrid.getCells());
+    myTestGrid.computeStates();
+    nextStates = (ArrayList) myTestGrid.representStatesAsList(myTestGrid.getCells());
+    areEqual = true;
+    for (int i = 0; i < prevStates.size(); i++) {
+      areEqual = areEqual && (prevStates.get(i) == nextStates.get(i));
+    }
+    //Unstable input config
+    assertFalse(areEqual);
+
   }
 }
