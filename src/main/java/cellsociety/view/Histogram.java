81509620b9470abc224bb09ac765d93b541e83cf
Eka Ebong <eae25@duke.edu>
2022-10-30 23:13:57 -0400

    refactoring

--- src/main/java/cellsociety/view/Histogram.java
+++ src/main/java/cellsociety/view/Histogram.java
@@ -5,15 +5,8 @@ import java.util.Set;
 
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
-<<<<<<< HEAD
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
-=======
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
->>>>>>> efdab70d1bb75822ca3a41ebf2194db69fc8cb63
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -33,18 +26,10 @@ public class Histogram{
         myStage.setTitle("Current Cell Numbers");
         myGrid = theGrid;
         cellNumbers = myGrid.getCurrentStates();
-<<<<<<< HEAD
     }
 
 
     public void makeBarGraph(){
-=======
-        System.out.println("created Histo");
-    }
-
-
-    public void createGraph(){
->>>>>>> efdab70d1bb75822ca3a41ebf2194db69fc8cb63
         myStage.show();
         series1 = new XYChart.Series<>();
         cellKeys = cellNumbers.keySet();
@@ -61,41 +46,22 @@ public class Histogram{
         barChart.getData().addAll(series1);
         xAxis.setLabel("State");
         yAxis.setLabel("Cell Population");
-<<<<<<< HEAD
-=======
-        System.out.println(barChart);
->>>>>>> efdab70d1bb75822ca3a41ebf2194db69fc8cb63
         myPane.getChildren().add(barChart);
         Scene scene = new Scene(myPane, 600, 600);
         myStage.setScene(scene);
         myStage.show();
     }
 
-<<<<<<< HEAD
     public void updateBarGraph(){
-=======
-    public void updateGraph(){
->>>>>>> efdab70d1bb75822ca3a41ebf2194db69fc8cb63
         cellKeys.forEach( (n) -> {
             series1.getData().add(new XYChart.Data<String, Integer>("State " + n, (Integer) cellNumbers.get(n), true));
         });
-        
+
     }
 
     public void shutDown(){
         myStage.close();
     }
 
-<<<<<<< HEAD
-=======
-    public void testGrid(){
-        cellNumbers.put(0,1);
-        Text hghg = new Text("mgm");
-        Pane hgh = new Pane();
-        Scene scene = new Scene(hgh, 600, 600);
-        myStage.setScene(scene);
-        myStage.show();
-    }
->>>>>>> efdab70d1bb75822ca3a41ebf2194db69fc8cb63
 
 }
