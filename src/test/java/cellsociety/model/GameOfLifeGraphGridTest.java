3d86771c56d590cc5950bd2a747cb7f6a9f56974
Jake Vigeant <jmv43@duke.edu>
2022-10-30 21:56:51 -0400

    game of life grid testing

--- src/test/java/cellsociety/model/GameOfLifeGraphGridTest.java
+++ src/test/java/cellsociety/model/GameOfLifeGraphGridTest.java
@@ -1,39 +1,33 @@
 package cellsociety.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import cellsociety.model.grids.FireGraphGrid;
+import cellsociety.model.grids.GameOfLifeGraphGrid;
 import cellsociety.parser.CSVParser;
 import cellsociety.view.GridWrapper;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.Properties;
 import org.junit.jupiter.api.Test;
 
-public class FireGraphGridTest {
+public class GameOfLifeGraphGridTest {
   CSVParser myGridParser = new CSVParser();
-  GridWrapper gridWrapper = myGridParser.parseData("fire/fire_simple_test.csv");
-
+  GridWrapper gridWrapper = myGridParser.parseData("game_of_life/glider.csv");
+  Properties p = new Properties();
   @Test
-  public void testStateComputation(){
-    Properties p = new Properties();
-    p.setProperty("Type", "Fire");
-    p.setProperty("Parameter", "1");
-    FireGraphGrid myTestGrid = new FireGraphGrid(gridWrapper,p);
+  public void testStateComputation()
+  {
+    p.setProperty("Type", "GameOfLife");
+    GameOfLifeGraphGrid myTestGrid = new GameOfLifeGraphGrid(gridWrapper, p);
     ArrayList<Integer> prevStates = new ArrayList<>();
     ArrayList<Integer> nextStates = new ArrayList<>();
     prevStates = (ArrayList) myTestGrid.representStatesAsList(myTestGrid.getCells());
     myTestGrid.computeStates();
     nextStates = (ArrayList) myTestGrid.representStatesAsList(myTestGrid.getCells());
     boolean areEqual = true;
-    for(int i = 0; i < prevStates.size(); i++){
+    for (int i = 0; i < prevStates.size(); i++) {
       areEqual = areEqual && (prevStates.get(i) == nextStates.get(i));
     }
     assertFalse(areEqual);
-
   }
-
 }
