1dea20e1422a629978d12f7fa00cc49424e54cd5
Eka Ebong <eae25@duke.edu>
2022-10-31 02:00:41 -0400

    working line and bar graph

--- src/main/java/cellsociety/view/GridScreen.java
+++ src/main/java/cellsociety/view/GridScreen.java
@@ -274,6 +274,7 @@ public class GridScreen extends SceneCreator {
   }
 
   private void goBack() {
+    closeGraph();
     FileInput backInput = new FileInput(600, myStage);
     myStage.setScene(backInput.createScene(getLanguage(), FILE_INPUT_CSS));
   }
@@ -292,6 +293,7 @@ public class GridScreen extends SceneCreator {
   }
 
   private void exitSimulation() {
+    closeGraph();
     SplashScreen beginning = new SplashScreen(600.0, myStage);
     myStage.setScene(beginning.createScene(START_SPLASH_CSS));
   }
@@ -319,11 +321,28 @@ public class GridScreen extends SceneCreator {
   public GridView getCurrentView(){return gridView;}
 
   private void createBarGraph() {
+    closeGraph();
     myChart = new Histogram(gridView);
     myChart.makeBarGraph();
     timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(refreshRate), e -> {
       if(myChart!=null){myChart.updateBarGraph();}
     }));
+  }
+
+  private void createLineGraph() {
+    closeGraph();
+    myChart = new Histogram(gridView);
+    myChart.makeLineGraph(timeline);
+    timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(refreshRate), e -> {
+      if(myChart!=null){myChart.updateLineGraph(timeline);}
+    }));
+  }
+
+  private void closeGraph(){
+    if(myChart!=null){
+      myChart.shutDown();
+      timeline.getKeyFrames().remove(0);
+    }
   }
 
 }
--- src/main/java/cellsociety/view/Histogram.java
+++ src/main/java/cellsociety/view/Histogram.java
@@ -1,16 +1,16 @@
 package cellsociety.view;
 
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
+import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
 
 
 public class Histogram{
@@ -18,48 +18,122 @@ public class Histogram{
     private HashMap cellNumbers;
     private GridView myGrid;
     private BarChart barChart;
-    private XYChart.Series<String, Integer> series1;
+    private LineChart lineChart;
+    private XYChart.Series<String, Integer> barSeries;
+    private XYChart.Series<Number, Integer> lineSeries1;
+    private XYChart.Series<Number, Integer> lineSeries2;
+    private XYChart.Series<Number, Integer> lineSeries3;
+    List<XYChart.Series<Number,Integer>> SERIES_LIST;
     private Set cellKeys;
     private FlowPane myPane;
+    private int updateTime;
+
+    /**
+     * Sets up the main variable for the function of the Histogram class
+     * @param theGrid
+     */
     public Histogram(GridView theGrid){
         myStage = new Stage();
         myStage.setTitle("Current Cell Numbers");
         myGrid = theGrid;
         cellNumbers = myGrid.getCurrentStates();
+        cellKeys = cellNumbers.keySet();
     }
 
 
+    /**
+     * Createes the Bar Graph
+     */
     public void makeBarGraph(){
-        myStage.show();
-        series1 = new XYChart.Series<>();
-        cellKeys = cellNumbers.keySet();
+        //create new series
+        setSeries();
 
-        series1.setName("Real Data");
         cellKeys.forEach( (n) -> {
-            series1.getData().add(new XYChart.Data<String, Integer>("State " + n, (Integer) cellNumbers.get(n), true));
+            barSeries.getData().add(new XYChart.Data<String, Integer>("State " + n, (Integer) cellNumbers.get(n), true));
         });
-
-        myPane = new FlowPane();
         CategoryAxis xAxis = new CategoryAxis();
         NumberAxis yAxis = new NumberAxis();
-        barChart = new BarChart(xAxis, yAxis);
-        barChart.getData().addAll(series1);
+
         xAxis.setLabel("State");
         yAxis.setLabel("Cell Population");
+
+        myPane = new FlowPane();
+
+        barChart = new BarChart(xAxis, yAxis);
+        barChart.getData().addAll(barSeries);
+
         myPane.getChildren().add(barChart);
+
+        showGraph();
+    }
+
+    //Sets the stage and shows it with the graph
+
+    private void showGraph(){
         Scene scene = new Scene(myPane, 600, 600);
         myStage.setScene(scene);
+        myStage.setX(0);
+        myStage.setY(0);
         myStage.show();
     }
 
-    public void updateBarGraph(){
+    //Initializes the 4 series needed for this class
+    private void setSeries(){
+        barSeries = new XYChart.Series<>();
+        lineSeries1 = new XYChart.Series<>();
+        lineSeries2 = new XYChart.Series<>();
+        lineSeries3 = new XYChart.Series<>();
+    }
+
+    /**
+     * creates a line graph using the parameter time to create x axis
+     * @param time
+     */
+    public void makeLineGraph(Timeline time){
+        setSeries();
+        SERIES_LIST = List.of(lineSeries1, lineSeries2, lineSeries3);
         cellKeys.forEach( (n) -> {
-            series1.getData().add(new XYChart.Data<String, Integer>("State " + n, (Integer) cellNumbers.get(n), true));
+
+            SERIES_LIST.get((Integer) n).getData().add(new XYChart.Data<Number, Integer>(0, (Integer) cellNumbers.get(n), true));
+        });
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        myPane = new FlowPane();
+        lineChart = new LineChart(xAxis, yAxis);
+        lineChart.getData().addAll(lineSeries1, lineSeries2, lineSeries3);
+        myPane.getChildren().add(lineChart);
+        showGraph();
+    }
+
+    /**
+     * updates bar graph by replacing series
+     */
+    public void updateBarGraph(){
+        barSeries.getData().clear();
+        cellKeys.forEach( (n) -> {
+            barSeries.getData().add(new XYChart.Data<String, Integer>("State " + n, (Integer) cellNumbers.get(n), true));
         });
 
     }
 
+    /**
+     * updates line graph by adding a data point every time the grid is refreshed
+     * @param timeline
+     */
+
+    public void updateLineGraph(Timeline timeline){
+        updateTime += 1;
+        cellKeys.forEach( (n) -> {
+
+            SERIES_LIST.get((Integer) n).getData().add(new XYChart.Data<Number, Integer>(updateTime, (Integer) cellNumbers.get(n), true));
+        });
+    }
+
+    /**
+     * used to close the stage 
+     */
     public void shutDown(){
+        setSeries();
         myStage.close();
     }
 
